02c91ef2538106b899ff62c04b6068aba12805b6
Hrihexaphor <hritesh.hexaphor@gmail.com>
2025-04-23 11:35:20 +0530

    comit for blog image

--- backend/middleware/blogUpload.js
+++ backend/middleware/blogUpload.js
@@ -1,15 +1,5 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../utils/cloudinary.js'; // adjust path if needed
-
-const blogStorage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: 'realestate_blogs',
-    allowed_formats: ['jpg', 'jpeg', 'png'],
-    public_id: (req, file) => `${Date.now()}-${file.originalname.split('.')[0]}`,
-  },
-});
+import blogStorage from '../utils/cloudinary.js'; // adjust path if needed
 
 const uploadBlogImage = multer({ storage: blogStorage });
 
--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,8 +1,18 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-  
-  export default cloudinary;
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const blogStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'realestate/blogs',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+  },
+});
+
+export default { cloudinary, blogStorage };
