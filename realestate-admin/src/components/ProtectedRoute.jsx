24a494ffbe944ce6a37002eada980af2db9ed21e
Hritik R <35923605+HritikR@users.noreply.github.com>
2025-05-17 19:31:57 +0530

    Show Loader while checking session

--- realestate-admin/src/components/ProtectedRoute.jsx
+++ realestate-admin/src/components/ProtectedRoute.jsx
@@ -8,14 +8,21 @@ const ProtectedRoute = ({ children }) => {
   useEffect(() => {
     async function loadAuth() {
       const userData = await checkSession();
-      setSession({ user: userData});
+      setSession({ user: userData });
     }
 
     loadAuth();
   }, []);
 
   if (!session) {
-    return <div>Loading...</div>
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <svg className="animate-spin h-14 w-14 text-blue-600 mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
+        </svg>
+      </div>
+    )
   }
 
   if (!session.user) {
